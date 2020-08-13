53c1f870a0703cbf143ab3cebda894ba06643a56
dkasatchkow <d.kasatchkow@gmail.com>
2020-08-13 19:02:44 +0800

    Added index.html

--- brandnewfile.py
+++ brandnewfile.py
@@ -1,1 +1,10 @@
-api_key="my special key"
+def print_hello():
+    print("hi")
+
+print_hello()
+
+def another_function(func):
+    print("Another function")
+    func()
+
+another_function(print_hello)
--- file_name.py
+++ file_name.py
@@ -1,1 +1,10 @@
-gfdgdfhdne
+class Dog():
+    def __init__(self, name, color):
+        super().__init__()
+        self.name = name
+        self.color = color
+
+dog = Dog('Fido','Brown')
+
+print(dog.name)
+print(dog.color)
