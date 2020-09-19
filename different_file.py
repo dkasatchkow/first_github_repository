ba00347f266b0f17a6b10d99064931c1b6478910
dkasatchkow <d.kasatchkow@gmail.com>
2020-09-19 18:03:58 +0800

    commiting changes

--- different_file.py
+++ different_file.py
@@ -1,1 +1,31 @@
-print("something")
+def scraper():
+    pass
+    # <code here>
+
+    # first scrape headlines
+    # variables: headline and subheadlines
+    scraped_headline, subheadline = some_function()
+    # scrape picture
+    picture_string = function_1()
+    # scrape table
+    # scrape hemispheres
+
+    def function_in_a_function():
+        return something
+
+
+    return final_dictionary
+
+def some_function():
+    # pass
+    # code to do scraping
+    return (headline, subheadline)
+
+def function_1():
+    return
+
+def function_2():
+    return
+
+# if __name__=="__main__":
+#     run_tests()
